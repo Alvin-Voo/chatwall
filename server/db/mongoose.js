90e0b46eb0ba9a3c3fc5cf46d67af83c2faca250
Alvin Voo <alvin.voo@gmail.com>
2018-06-08 15:57:31 +0800

    fixing mongoose syntax for Mongo Atlas

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dbdomain = process.env.MONGODB_URI;
+const dbname = process.env.MONGODB_DBNAME;
 
-mongoose.connect(dbdomain);
+if(dbname){
+  mongoose.connect(dbdomain,{dbName: dbname})
+}
+else mongoose.connect(dbdomain);
+
+
 
 module.exports = mongoose;
